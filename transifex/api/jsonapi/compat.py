e4253731d52f8b9d32bc6fd199615fee13f9538c
Konstantinos Bairaktaris <kbairak@transifex.com>
2021-09-09 09:21:33 +0300

    Compatibility issues with JSONDecodeError
    `requests` is somewhat inconsistent with how it raises this exception.
    (https://github.com/psf/requests/issues/5794)
    
    Depending on the environment, the following can happen during
    `response.json`:
    
      - Python2 and no `simplejson`, a `ValueError` will be raised
      - Python2 and `simplejson`, a `simplejson.JSONDecodeError` will be
        raised
      - Python3 and no `simplejson`, a `json.JSONDecodeError` will be raised
      - Python3 and `simplejson`, a `simplejson.JSONDecodeError` will be
        raised
    
    The following wil make sure that catching
    `transifex.api.jsonapi.compat.JSONDecodeError` in a `try:
    response.json()`
    block will always work

--- transifex/api/jsonapi/compat.py
+++ transifex/api/jsonapi/compat.py
@@ -2,10 +2,33 @@ from __future__ import absolute_import, unicode_literals
 
 import json
 
+# `requests` is somewhat inconsistent with how it raises this exception.
+# (https://github.com/psf/requests/issues/5794)
+#
+# Depending on the environment, the following can happen during
+# `response.json`:
+#
+#   - Python2 and no `simplejson`, a `ValueError` will be raised
+#   - Python2 and `simplejson`, a `simplejson.JSONDecodeError` will be raised
+#   - Python3 and no `simplejson`, a `json.JSONDecodeError` will be raised
+#   - Python3 and `simplejson`, a `simplejson.JSONDecodeError` will be raised
+#
+# The following wil make sure that catching
+# `transifex.api.jsonapi.compat.JSONDecodeError` in a `try: response.json()`
+# block will always work
+JSONDecodeError = []
 try:
-    JSONDecodeError = json.JSONDecodeError
+    JSONDecodeError.append(json.JSONDecodeError)
 except AttributeError:
-    JSONDecodeError = ValueError
+    pass
+try:
+    import simplejson
+    JSONDecodeError.append(simplejson.JSONDecodeError)
+except ImportError:
+    pass
+if not JSONDecodeError:
+    JSONDecodeError = [ValueError]
+JSONDecodeError = tuple(JSONDecodeError)
 
 try:
     import collections.abc as abc
