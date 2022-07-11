3195018f5093d901e456971b0726a979ed471a3a
Konstantinos Bairaktaris <kbairak@transifex.com>
2022-07-11 15:31:49 +0300

    Use txlint for code quality checks

--- tests/common/test_strings.py
+++ tests/common/test_strings.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 import sys
 
 import pytest
-from transifex.common.strings import (LazyString, alt_quote,
-                                      printf_to_format_style)
+
+from transifex.common.strings import LazyString, alt_quote, printf_to_format_style
 
 
 def test_printf_to_format_style():
